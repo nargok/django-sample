ee715237cf9d7f042ece508b7f3313c9ffcc460e
Ryoichi Gokan <nargok@gmail.com>
2018-06-09 16:48:50 +0900

    message template

--- item/views.py
+++ item/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
 # Create your views here.
 def hello(request):
-    return HttpResponse('Hello World')
+    context = {'message': 'メッセージ'}
+    return TemplateResponse(request, 'item/message.html', context=context)
