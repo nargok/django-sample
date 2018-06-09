a3337256490a22955440438a9601c3d5159302ef
Ryoichi Gokan <nargok@gmail.com>
2018-06-09 17:00:43 +0900

    display headers information

--- item/views.py
+++ item/views.py
@@ -3,5 +3,17 @@ from django.template.response import TemplateResponse
 
 # Create your views here.
 def hello(request):
-    context = {'message': 'メッセージ'}
-    return TemplateResponse(request, 'item/message.html', context=context)
+    context = {
+      'headers': {
+        'scheme': request.scheme,
+        'path': request.path,
+        'method': request.method,
+        'content_length': request.META['CONTENT_LENGTH'],
+        'http_accept': request.META['HTTP_ACCEPT'],
+        'http_accept_language': request.META['HTTP_ACCEPT_LANGUAGE'],
+        'user_agent': request.META['HTTP_USER_AGENT'],
+        'remote_addr': request.META['REMOTE_ADDR'],    
+      }
+    }
+
+    return TemplateResponse(request, 'item/header.html', context)
